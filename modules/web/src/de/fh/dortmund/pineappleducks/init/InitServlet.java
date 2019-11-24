bb611ea6ce2cacdedecb0f8b041ebc35974c008c
Etti <44148320+Ettebumms@users.noreply.github.com>
2019-11-24 19:20:56 +0100

    init try 1

--- modules/web/src/de/fh/dortmund/pineappleducks/init/InitServlet.java
+++ modules/web/src/de/fh/dortmund/pineappleducks/init/InitServlet.java
@@ -6,13 +6,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(value = "/init")
 public class InitServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        System.out.println("hi");
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html><head><title>helloworld</title></head><body>");
+        writer.println("<h1>Hello World</h1>");
+        writer.println("</body></html>");
+        writer.close();
+
+//        super.doGet(req, resp);
     }
+
 
 }
