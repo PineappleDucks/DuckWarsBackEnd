e27ee74ffa4345a7e7fdab2dba2cd56b7e90a171
Shiroukoji <shiroukoji@posteo.de>
2020-01-15 22:46:45 +0100

    implement Register

--- modules/ejb/src/main/java/de/fh/dortmund/pineappleducks/register/RegisterBean.java
+++ modules/ejb/src/main/java/de/fh/dortmund/pineappleducks/register/RegisterBean.java
@@ -1,5 +1,6 @@
 package de.fh.dortmund.pineappleducks.register;
 
+import de.fh.dortmund.pineappleducks.entity.User;
 import de.fh.dortmund.pineappleducks.manager.UserManager;
 
 import javax.ejb.Stateless;
@@ -13,6 +14,12 @@ public class RegisterBean implements RegisterBeanRemote, RegisterBeanLocal{
     public String persist ( String login, String password) {
         UserManager manager = new UserManager();
         manager.registerUser(login, password);
-        return manager.getUserByName(login).getPassword();
+        User savedUser = manager.getUserByName(login);
+        if(savedUser.getUsername().equals(login)
+        && savedUser.getPassword().equals(password)){
+            return "Die Registrierung war erfolgreich!";
+        }else {
+            throw new RuntimeException("Es ist ein Fehler bei der Persistenz aufgetreten!");
+        }
     }
 }
