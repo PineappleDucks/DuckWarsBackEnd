b93e1aab0c83e6d4e96a965935f6c0778d98e4c4
Sebastian Loer <seba.loer@t-online.de>
2020-01-15 14:26:11 +0100

    fix servlet login to no error

--- modules/web/src/main/java/de/fh/dortmund/pineappleducks/loginEJB/LoginServlet.java
+++ modules/web/src/main/java/de/fh/dortmund/pineappleducks/loginEJB/LoginServlet.java
@@ -1,27 +1,18 @@
 package de.fh.dortmund.pineappleducks.loginEJB;
 
+import com.google.gson.Gson;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
-    Class Resp{
-        String id;
-        Date created;
-        String userId;
-                    public Resp(String id, String user){
-            created = new Date();
-            created = created.getTime();
-            this.id = id;
-            userId = user;
-        }
-    }
-    int respId = 1;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -30,24 +21,18 @@ public class LoginServlet extends HttpServlet {
         String password = req.getParameter("password");
 
         Login login = new Login(username, password);
-        if(login.checkName()==true){
+        if(login.checkName()){
             System.err.println("Fehler, der Name "+username+" ist schon vergeben");
         }
         else{
          login.register();
-            Resp resp = new Resp(Integer.toString(respId), username)
-                    respId++;
-            try {
-                Gson gson = new Gson();
-                String loginString = gson.toJson(resp);
-                PrintWriter out = resp.getWriter();
-                resp.setContentType("application/json");
-                resp.setCharacterEncoding("UTF-8");
-                out.print(loginString);
-                out.flush();
-            } catch (ConstructException e) {
-                System.err.println(e.getMessage());
-            }
+            Gson gson = new Gson();
+            String loginString = gson.toJson(resp);
+            PrintWriter out = resp.getWriter();
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            out.print(loginString);
+            out.flush();
         }
 
     }
@@ -58,20 +43,14 @@ public class LoginServlet extends HttpServlet {
         String password = req.getParameter("password");
 
         Login login = new Login(username, password);
-        if (login.checkLogin() == true) {
-            Resp resp = new Resp(Integer.toString(respId), username)
-            respId++;
-            try {
-                Gson gson = new Gson();
-                String loginString = gson.toJson(resp);
-                PrintWriter out = resp.getWriter();
-                resp.setContentType("application/json");
-                resp.setCharacterEncoding("UTF-8");
-                out.print(loginString);
-                out.flush();
-            } catch (ConstructException e) {
-                System.err.println(e.getMessage());
-            }
+        if (login.checkLogin()) {
+            Gson gson = new Gson();
+            String loginString = gson.toJson(resp);
+            PrintWriter out = resp.getWriter();
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            out.print(loginString);
+            out.flush();
         }
     }
 }
