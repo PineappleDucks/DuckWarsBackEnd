b78803a613281654c35319b37d9398a5cb449cdd
Shiroukoji <shiroukoji@posteo.de>
2020-01-15 22:50:18 +0100

    add imports

--- modules/web/src/main/java/de/fh/dortmund/pineappleducks/loginEJB/LoginServlet.java
+++ modules/web/src/main/java/de/fh/dortmund/pineappleducks/loginEJB/LoginServlet.java
@@ -1,11 +1,14 @@
 package de.fh.dortmund.pineappleducks.loginEJB;
 
+import com.google.gson.Gson;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 
 @WebServlet("/login")
