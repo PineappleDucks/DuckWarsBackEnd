8fec85e767084465abc7853b37d11cfc0ae5a29c
Ole Hemsing <ole.hemsing@gmx.net>
2020-01-20 03:22:58 +0100

    Login Schnittstelle

--- modules/web/src/main/java/de/fh/dortmund/pineappleducks/loginEJB/LoginServlet.java
+++ modules/web/src/main/java/de/fh/dortmund/pineappleducks/loginEJB/LoginServlet.java
@@ -1,65 +1,25 @@
-package de.fh.dortmund.pineappleducks.loginEJB;
+package de.fh.dortmund.pineappleducks.login;
 
-import com.google.gson.Gson;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-
+        import javax.ejb.EJB;
+        import javax.servlet.ServletException;
+        import javax.servlet.annotation.WebServlet;
+        import javax.servlet.http.HttpServlet;
+        import javax.servlet.http.HttpServletRequest;
+        import javax.servlet.http.HttpServletResponse;
+        import java.io.IOException;
+        import java.io.PrintWriter;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
+    @EJB
+    LoginBeanLocal loginBeanLocal;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        String username = req.getParameter("username");
-        String password = req.getParameter("password");
-
-        Login login = new Login(username, password);
-        if (login.checkName()) {
-            System.err.println("Fehler, der Name " + username + " ist schon vergeben");
-        } else {
-            try {
-                login.register();
-                Gson gson = new Gson();
-                String loginString = gson.toJson(resp);
-                PrintWriter out = resp.getWriter();
-                resp.setContentType("application/json");
-                resp.setCharacterEncoding("UTF-8");
-                out.print(loginString);
-                out.flush();
-            } catch (ArrayIndexOutOfBoundsException e) {
-                e.printStackTrace();
-                System.err.println("Anfrage enthaelt Fehler!");
-            }
-        }
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String username = req.getParameter("name");
-        String password = req.getParameter("password");
-
-        Login login = new Login(username, password);
-        if (login.checkLogin()) {
-
-            try {
-                Gson gson = new Gson();
-                String loginString = gson.toJson(resp);
-                PrintWriter out = resp.getWriter();
-                resp.setContentType("application/json");
-                resp.setCharacterEncoding("UTF-8");
-                out.print(loginString);
-                out.flush();
-            } catch (ArrayIndexOutOfBoundsException e) {
-                e.printStackTrace();
-                System.err.println("Anfrage enthaelt Fehler!");
-            }
-        }
+        PrintWriter writer = resp.getWriter();
+        //String login = req.getParameter("login");
+        //String password = req.getParameter("password");
+        writer.print(loginBeanLocal.persist("hallo", "hallo"));
     }
 }
