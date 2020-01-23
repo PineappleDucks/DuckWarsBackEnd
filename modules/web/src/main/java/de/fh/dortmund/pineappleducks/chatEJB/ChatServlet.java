ac8ae12ba227ef6f7fc0caf97e318695cbbbe921
Shiroukoji <shiroukoji@posteo.de>
2020-01-23 18:09:55 +0100

    session for all servlets

--- modules/web/src/main/java/de/fh/dortmund/pineappleducks/chatEJB/ChatServlet.java
+++ modules/web/src/main/java/de/fh/dortmund/pineappleducks/chatEJB/ChatServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -18,36 +19,56 @@ import static java.lang.Integer.parseInt;
 public class ChatServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-// fragt Antwort-Optionen für den Nutzer ab
-        int messageId = parseInt(req.getParameter("messageId"));
-        Chat chat = new Chat();
-        Gson gson = new Gson();
-        Message[] ant = chat.getOptions(messageId); // get mögliche Antworten
+        HttpSession session = req.getSession(false);
+        if(session != null){
+            // fragt Antwort-Optionen für den Nutzer ab
+            int messageId = parseInt(req.getParameter("messageId"));
+            Chat chat = new Chat();
+            Gson gson = new Gson();
+            Message[] ant = chat.getOptions(messageId); // get mögliche Antworten
 
-        PrintWriter out = resp.getWriter();
-        resp.setContentType("application/json");
-        resp.setCharacterEncoding("UTF-8");
+            PrintWriter out = resp.getWriter();
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
 
-        out.print(gson.toJson(ant));
+            out.print(gson.toJson(ant));
 
-        out.flush();
+            out.flush();
+        }else{
+            PrintWriter out = resp.getWriter();
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            resp.setStatus(403);
+            out.print("unauthorized");
+            out.flush();
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // schickt Antwort des Nutzers, erhält Antwort des Servers zurück
-        int messageId = parseInt(req.getParameter("messageId"));
-        int chatId = parseInt(req.getParameter("chatId"));
-        Chat chat = new Chat();
-        Gson gson = new Gson();
-        ServerMessage messages = chat.nachrichtSenden(chatId, messageId);
+        HttpSession session = req.getSession(false);
+        if(session != null) {
+            // schickt Antwort des Nutzers, erhält Antwort des Servers zurück
+            int messageId = parseInt(req.getParameter("messageId"));
+            int chatId = parseInt(req.getParameter("chatId"));
+            Chat chat = new Chat();
+            Gson gson = new Gson();
+            ServerMessage messages = chat.nachrichtSenden(chatId, messageId);
 
-        PrintWriter out = resp.getWriter();
-        resp.setContentType("application/json");
-        resp.setCharacterEncoding("UTF-8");
+            PrintWriter out = resp.getWriter();
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
 
-        out.print(gson.toJson(messages));
+            out.print(gson.toJson(messages));
 
-        out.flush();
+            out.flush();
+        }else{
+            PrintWriter out = resp.getWriter();
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            resp.setStatus(403);
+            out.print("unauthorized");
+            out.flush();
+        }
     }
 }
