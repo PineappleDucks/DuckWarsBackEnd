efd896647b590b15a9f13524f4771f8760cce637
Shiroukoji <shiroukoji@posteo.de>
2020-01-23 16:56:32 +0100

    reimplement login with Java EE

--- modules/web/src/main/java/de/fh/dortmund/pineappleducks/register/RegisterServlet.java
+++ modules/web/src/main/java/de/fh/dortmund/pineappleducks/register/RegisterServlet.java
@@ -18,41 +18,21 @@ public class RegisterServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-
-        // get params
         String username = req.getParameter("name");
         String password = req.getParameter("password");
-
         if (registerBeanLocal.checkForExistingUserName(username) == Boolean.FALSE) {
-
-            // register
             registerBeanLocal.persist(username, password);
-
-            // new gson class for output
             Gson gson = new Gson();
-
-            // get writer
             PrintWriter out = resp.getWriter();
-
-            // set output metadata
             resp.setContentType("application/json");
             resp.setCharacterEncoding("UTF-8");
-
-            // print output
             out.print("register correct");
             out.flush();
         } else {
-            // username is used
-
-            // get writer
             PrintWriter out = resp.getWriter();
-
-            // set output metadata
             resp.setContentType("application/json");
             resp.setCharacterEncoding("UTF-8");
             resp.setStatus(403);
-
-            // print output
             out.print("username in use");
             out.flush();
 
